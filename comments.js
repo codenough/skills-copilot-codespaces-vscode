5f69dbd24b747a761739b618e7578f97c1137b99
Aleksandr Pototskiy <codenough@gmail.com>
2024-05-23 16:03:57 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,12 @@
+// Create WEB server
+
+var express = require('express');
+var app = express();
+
+app.get('/comments', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
